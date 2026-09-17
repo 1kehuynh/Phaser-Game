4e33e05c9f8978f54c99bd528aa5bfccc03830b5
Kenny Huynh <1kehuynh@Kennys-MacBook-Pro.local>
2026-09-16 22:49:47 -0700

    added whip animation, am working on fixing it. also tweaked bat animation

--- src/game.js
+++ src/game.js
@@ -14,15 +14,15 @@ export default class main extends Phaser.Scene {
         this.load.image('chatgpt2', 'chatgpt2.png');
         this.load.image('bat', 'bat.png')
         this.load.image('ow', 'ow.png')
+        this.load.spritesheet('whipanim', 'whipsheet2.png', {
+            frameWidth: 600,
+            frameHeight: 600
+        });
     }
 
     create ()
     {   
-        
         this.weapon = 'none';
-       
-        
-        
         this.anims.create(
             {
                 key: 'twerk',
@@ -34,97 +34,162 @@ export default class main extends Phaser.Scene {
                 repeat: -1,
             }
         )
-        const bot = this.add.sprite(740, 500, 'chatgpt').play('twerk').setInteractive().on('pointerdown', () => {
+
+        this.whip = new weapon(this, 'whip')
+        this.bat = new weapon(this, 'bat')
+
+        const bot = this.add.sprite(640, 500, 'chatgpt').play('twerk').setInteractive().on('pointerdown', () => {
             if(this.weapon != 'none'){
-                bot.setTexture('ow')
-                bot.setTint('0xff0000')
-                this.time.delayedCall(250, () => {
-                    bot.clearTint(); 
-                });
+                this.whip.once('animationupdate', (animation, frame) => {
+                    if (frame.index === 7) {
+                        bot.setTexture('ow')
+                        bot.setTint('0xff0000')
+                        
+                        const text = this.add.text(this.input.activePointer.x + (20 * Math.random()), this.input.activePointer.y - (20 * Math.random()), 'Work Faster!!!').setDepth(2).setColor('#000000')
+                        this.tweens.add({ 
+                            targets: text, 
+                            y: text.y - 50,
+                            alpha: 0,    
+                            duration: 500,   
+                            ease: 'Linear',
+                            onComplete: () => {text.destroy();},
+                        }); 
+                        this.time.delayedCall(100, () => {
+                            bot.clearTint(); 
+                        });
+                    }
+                    
+                }) 
+                        
             }
-        });
+        }).setOrigin(0.5);
+
+
         
-        const whipButton = new button(this, 30, 30, 'whip', (btn) => {
-            if(this.weapon === 'whip'){
-                this.whip.setActive(false)
-                this.whip.setVisible(false)
-                this.weapon = 'none';
-                btn.icon.setVisible(true);
-            } else {
-                this.whip.setActive(true)
-                this.whip.setVisible(true)
-                this.weapon = 'whip';
-                btn.icon.setVisible(false);
+
+        this.whipButton = new button(this, 30, 30, {type: 'weapon', weaponType: this.whip, siblings: []})
+        this.batButton = new button(this, 100, 30, {type: 'weapon', weaponType: this.bat, siblings: []})
+        this.whipButton.siblings = [this.batButton];
+        this.batButton.siblings = [this.whipButton];
+
+
+        this.input.on('pointerdown', () => {
+            if(this.weapon === this.bat){
+                this.bat.anim();
+            } else if(this.weapon === this.whip){
+               this.whip.anim();
             }
         })
-
-        const batButton = new button(this, 90, 30, 'bat', (btn) => {
-            if(this.weapon === 'bat'){
-                this.bat.setActive(false)
-                this.bat.setVisible(false)
-                this.weapon = 'none';
-                btn.icon.setVisible(true);
-            } else {
-                this.bat.setActive(true)
-                this.bat.setVisible(true)
-                this.weapon = 'bat';
-                btn.icon.setVisible(false);
-            }
-            
-        })
-
-
-        this.whip = this.add.sprite(200, 200, 'whip')
-        this.whip.setActive(false)
-        this.whip.setVisible(false)
-        
-        this.bat = this.add.sprite(200, 200, 'bat').setInteractive();
-        this.bat.setActive(false)
-        this.bat.setVisible(false)
-        this.bat.on('pointerdown', () => {
-            this.tweens.add({
-                targets: this.bat,
-                rotation: 50,
-                duration: 100,
-            })
-        })
-
     }
 
     update () 
     {   
-        if(this.weapon === 'whip'){
-            this.whip.setPosition(this.input.activePointer.x - 50,this.input.activePointer.y + 50);
+        if(this.weapon === this.whip && this.whip.anims.isPlaying == false){
+            this.whip.setPosition(this.input.activePointer.x - 70,this.input.activePointer.y + 50);
         } 
-        if(this.weapon === 'bat'){
-            this.bat.setPosition(this.input.activePointer.x - 50,this.input.activePointer.y + 50);
-        } 
-        console.log(this.weapon)
+        if(this.weapon === this.bat){
+            this.bat.setPosition(this.input.activePointer.x, this.input.activePointer.y);
+        }
+
+        
     }
 }
 
-class button extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, iconKey, callback) {
-        super(scene, x, y);
-
-        this.bg = scene.add.rectangle(x, y, 75, 75).setInteractive();
-        this.bg.setStrokeStyle(2, 0x1a65ac);
-        this.icon = scene.add.image(x, y, iconKey).setScale(0.5, 0.5);
-
-        this.add([this.bg, this.icon]);
-
+class weapon extends Phaser.GameObjects.Sprite{
+    constructor(scene, key){
+        super(scene, 0, 0, key);
+        this.setActive(false);
+        this.setVisible(false);
+        this.setDepth(1);
+        this.scene = scene;
         scene.add.existing(this);
+        this.key = key;
+        
+        if (!this.scene.anims.exists('do')) {
+            this.scene.anims.create({
+                key: 'do',
+                frames: this.scene.anims.generateFrameNumbers('whipanim', {
+                    start: 0,
+                    end: 7
+                }),
+                frameRate: 12,
+                repeat: 0 
+            });
+        }
+    }
 
-        this.bg.on('pointerover', () => {
-            scene.tweens.add({ targets: this, scale: 1.1, duration: 100 });
-        });
+    anim() {
+        if(this.key == 'whip'){
+            this.once('animationcomplete-do', () => {
+                this.setTexture('whip');
+            });
 
-        this.bg.on('pointerout', () => {
-            scene.tweens.add({ targets: this, scale: 1.0, duration: 100 });
-        });
+            this.play('do', true);
+        } else {
+            this.scene.tweens.add({
+                targets: this,
+                duration: 100,
+                rotation: Math.PI,
+                ease: 'linear',
+                onComplete: () => {
+                    this.setRotation(0)
+                }
+            })
+        }
+    }
+}
 
-        this.bg.on('pointerdown', () => {
-            callback(this);
-        });
+
+class button extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, config = {type, weaponType, siblings}) {
+        super(scene, x, y);
+        if(config.type === 'weapon'){
+            this.bg = scene.add.rectangle(x, y, 75, 75).setInteractive();
+            this.bg.setStrokeStyle(2, 0x1a65ac);
+            this.icon = scene.add.image(x, y, config.weaponType.texture).setScale(0.5, 0.5);
+
+            this.add([this.bg, this.icon]);
+
+            scene.add.existing(this);
+
+            this.bg.on('pointerover', () => {
+                scene.tweens.killTweensOf(this);
+                scene.tweens.add({ targets: this, scale: 1.1, duration: 100 });
+            });
+
+            this.bg.on('pointerout', () => {
+                scene.tweens.killTweensOf(this);
+                scene.tweens.add({ targets: this, scale: 1.0, duration: 100 });
+            });
+
+            this.bg.on('pointerdown', () => {
+                //test cases, weapon is none and user clicks button, weapon is bat or whip and user clicks same, weapon is bat or whip and user clicks different
+                //
+                try{
+                    scene.weapon.setVisible(false)
+                    scene.weapon.setActive(false)
+                    //if not clicking on same button as current weapon
+                    if(scene.weapon != config.weaponType){
+                        scene.weapon = config.weaponType;
+                        scene.weapon.setVisible(true)
+                        scene.weapon.setActive(true)
+                        this.bg.setFillStyle('0x808080');
+                        this.icon.setTint('0x808080')
+                        this.siblings[0].bg.setFillStyle();
+                        this.siblings[0].icon.clearTint();
+                    } else{ //if clicking on same button as current weapon
+                        scene.weapon = 'none'
+                        this.bg.setFillStyle();
+                        this.icon.clearTint();
+                    }
+                } catch {
+                    scene.weapon = config.weaponType;
+                    scene.weapon.setVisible(true)
+                    scene.weapon.setActive(true)
+                    this.bg.setFillStyle('0x808080');
+                    this.icon.setTint('0x808080')
+                }
+            });
+        }
     }
 }
